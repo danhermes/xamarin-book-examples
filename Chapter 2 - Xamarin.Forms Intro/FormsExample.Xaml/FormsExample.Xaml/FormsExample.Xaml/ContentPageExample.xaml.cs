4a1fa638fecc55f8430dc39633ee69c04ca1d14f
Jason Awbrey <jason@awbrey.net>
2015-04-07 13:32:19 -0500

    Xaml example for Chapter 2

---
+++ Chapter 2 - Xamarin.Forms Intro/FormsExample.Xaml/FormsExample.Xaml/FormsExample.Xaml/ContentPageExample.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FormsExample
+{
+	public partial class ContentPageExample : ContentPage
+	{
+		public ContentPageExample ()
+		{
+			InitializeComponent ();
+		}
+
+		protected void ButtonClicked(object sender, EventArgs e) {
+
+			((Button)sender).Text = "It is so!";
+		}
+
+		async protected void ImageTapped(object sender, EventArgs e) {
+
+			Image image = ((Image)sender);
+
+			image.Opacity = .5;
+			await Task.Delay(200);
+			image.Opacity = 1;
+		}
+	}
+}
