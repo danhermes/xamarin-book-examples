81197245d00d7a9111930ce1323e0656451d33ff
Jason Awbrey <jason@awbrey.net>
2015-05-24 16:34:38 -0500

    Xaml examples for Chapter 8

---
+++ Chapter 8 - Custom Renderers/CustomRenderer.Xaml/CustomRenderer.Xaml/CustomRenderer.Xaml/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CustomRenderer
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage ()
+		{
+			InitializeComponent ();
+		}
+
+		public void ButtonClicked(object sender, EventArgs args) {
+			DisplayAlert ("Congratulations", "This button was rendered in a platform-specific class.", "OK");
+		}
+	}
+}
+
---
+++ Chapter 8 - Custom Renderers/CustomRenderer.Xaml/CustomRenderer.Xaml/CustomRenderer.iOS/CustomButtonRenderer.cs
@@ -0,0 +1,23 @@
+﻿using Xamarin.Forms.Platform.iOS;
+using Xamarin.Forms;
+using UIKit;
+using CustomRenderer;
+using CustomRenderer.iOS;
+
+[assembly: ExportRenderer (typeof (CustomButton), typeof (CustomButtonRenderer))]
+
+namespace CustomRenderer.iOS
+{
+	public class CustomButtonRenderer : ButtonRenderer
+	{
+		protected override void OnElementChanged (ElementChangedEventArgs<Button> e)
+		{
+			base.OnElementChanged (e);
+
+			if (e.OldElement == null) {
+				Control.BackgroundColor = UIColor.Brown;
+			}
+		}
+	}
+}
+
