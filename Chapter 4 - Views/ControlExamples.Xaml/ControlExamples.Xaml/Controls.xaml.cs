32bd697c3acbd2d369dcc09c690496386ac74721
Alex Blount <alexb@alexs-macbook.local>
2015-06-02 23:33:27 +0100

    working on Chapter 5

--- Chapter 4 - Views/ControlExamples.Xaml/ControlExamples.Xaml/Controls.xaml.cs
+++ Chapter 4 - Views/ControlExamples.Xaml/ControlExamples.Xaml/Controls.xaml.cs
@@ -16,40 +16,40 @@ namespace ControlExamples.Xaml
 
         void SwitchToggled (object sender, ToggledEventArgs e)
         {
-            Label1.Text = String.Format("Switch is now {0}", e.Value ? "On" : "Off");
-            Label2.Text = TheSwitch.IsToggled.ToString();
+            EventValue.Text = String.Format("Switch is now {0}", e.Value ? "On" : "Off");
+            PageValue.Text = TheSwitch.IsToggled.ToString();
         }
 
         void SliderValueChanged (object sender, ValueChangedEventArgs e)
         {
-            Label1.Text = String.Format("Slider value is {0:F1}", e.NewValue);
-            Label2.Text = TheSlider.Value.ToString();
+            EventValue.Text = String.Format("Slider value is {0:F1}", e.NewValue);
+            PageValue.Text = TheSlider.Value.ToString();
         }
 
        
         void StepperValueChanged (object sender, ValueChangedEventArgs e)
         {
-            Label1.Text = String.Format("Stepper value is {0:F1}", e.NewValue);
-            Label2.Text = TheStepper.Value.ToString();
+            EventValue.Text = String.Format("Stepper value is {0:F1}", e.NewValue);
+            PageValue.Text = TheStepper.Value.ToString();
         }
 
         void TimePickerPropertyChanged (object sender, PropertyChangedEventArgs e)
         {
             if (e.PropertyName == TimePicker.TimeProperty.PropertyName)
             {
-                Label1.Text = TheTimePicker.Time.ToString();
+                EventValue.Text = TheTimePicker.Time.ToString();
             }
         }
 
         void DatePickerDateSelected (object sender, DateChangedEventArgs e)
         {
-            Label1.Text = e.NewDate.ToString();
-            Label2.Text = TheDatePicker.Date.ToString();
+            EventValue.Text = e.NewDate.ToString();
+            PageValue.Text = TheDatePicker.Date.ToString();
         }
 
         void PickerSelectedIndexChanged(object sender, EventArgs e)
         {
-            Label1.Text = ThePicker.Items[ThePicker.SelectedIndex];
+            EventValue.Text = ThePicker.Items[ThePicker.SelectedIndex];
         }
     }
 }
---
+++ Chapter 5 - Lists/ListViewExample.Xaml/ListViewExample.Xaml/HomePage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ListViewExample.Xaml
+{
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
