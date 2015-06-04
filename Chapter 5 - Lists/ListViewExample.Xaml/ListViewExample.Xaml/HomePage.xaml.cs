f058375eefd19a73bd6ad295574f50744ae2ead8
Alex Blount <alexb@alexs-macbook.home>
2015-06-04 12:15:49 +0100

    working on Chapter 5

--- Chapter 5 - Lists/ListViewExample.Xaml/ListViewExample.Xaml/HomePage.xaml.cs
+++ Chapter 5 - Lists/ListViewExample.Xaml/ListViewExample.Xaml/HomePage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 
 using Xamarin.Forms;
+using System.Windows.Input;
 
 namespace ListViewExample.Xaml
 {
@@ -10,6 +11,35 @@ namespace ListViewExample.Xaml
         public HomePage()
         {
             InitializeComponent();
+
+            BindingContext = new HomePageViewModel(Navigation);
+        }
+    }
+
+
+    public class HomePageViewModel : BindableObject
+    {
+
+        private readonly INavigation navigation;
+
+        public Command<string> NavigateCommand
+        {
+            get;
+            set;
+        }
+
+        public HomePageViewModel(INavigation navigation)
+        {
+            this.navigation = navigation;
+            NavigateCommand = new Command<string>(NagivigateRequested);
+        }
+
+
+        async void NagivigateRequested(string pageName)
+        {
+            var pageType = Type.GetType($"ListViewExample.Xaml.{pageName}");
+            Page page = (Page)Activator.CreateInstance(pageType);
+            await navigation.PushAsync(page);
         }
     }
 }
---
+++ Chapter 5 - Lists/ListViewExample.Xaml/ListViewExample.Xaml/ListViewStrings.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ListViewExample.Xaml
+{
+    public partial class ListViewStrings : ContentPage
+    {
+        public ListViewStrings()
+        {
+            InitializeComponent();
+
+            BindingContext = new ListViewStringsViewModel(DisplayAlert);
+        }
+    }
+
+    public class ListViewStringsViewModel : BindableObject
+    {
+        readonly Func<string, string, string, Task> displayAlertAction;
+
+        List<string> items;
+        public List<string> Items
+        {
+            get
+            {
+                return items;
+            }
+            set
+            {
+                items = value;
+                OnPropertyChanged("Items");
+            }
+        }
+
+        string selectedItem;
+        public string SelectedItem
+        {
+            get
+            {
+                return selectedItem;
+            }
+            set
+            {
+                selectedItem = value;
+                OnPropertyChanged("SelectedItem");
+                displayAlertAction?.Invoke("Tapped", $"{selectedItem} was selected.", "OK");
+            }
+        }
+
+        public ListViewStringsViewModel(Func<string, string, string, Task> displayAlertAction)
+        {
+            this.displayAlertAction = displayAlertAction;
+            Items = new List<string> 
+                {
+                    "First",
+                    "Second",
+                    "Third"
+                };
+        }
+    }
+}
+
