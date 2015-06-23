a96a7305c3e36bcca9f777992ea4d00bcf1be4b1
Alex Blount <alexb@alexs-macbook.local>
2015-06-23 12:45:10 +0100

    Custom with Image - Chapter 5

--- Chapter 5 - Lists/ListViewExample.Xaml/Droid/Resources/Resource.designer.cs
+++ Chapter 5 - Lists/ListViewExample.Xaml/Droid/Resources/Resource.designer.cs
@@ -45,7 +45,16 @@ namespace ListViewExample.Xaml.Droid
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int icon = 2130837504;
+			public const int first = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int icon = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int second = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int third = 2130837507;
 			
 			static Drawable()
 			{
