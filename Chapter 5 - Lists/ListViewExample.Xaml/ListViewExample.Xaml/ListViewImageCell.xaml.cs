291609fec8a0b4c9367f413e622b6a948a09882d
Alex Blount <alexb@alexs-macbook.local>
2015-06-23 13:24:11 +0100

    ImageCell Chapter 5

---
+++ Chapter 5 - Lists/ListViewExample.Xaml/ListViewExample.Xaml/ListViewImageCell.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ListViewExample.Xaml
+{
+    public partial class ListViewImageCell : ContentPage
+    {
+        public ListViewImageCell()
+        {
+            InitializeComponent();
+
+            BindingContext = new ListViewImageCellViewModel();
+        }
+    }
+
+    public class ListViewImageCellViewModel : BindableObject
+    {
+        List<ListItem> listItems;
+        public List<ListItem> ListItems
+        {
+            get
+            {
+                return listItems;
+            }
+            set
+            {
+                listItems = value;
+                OnPropertyChanged("ListItems");
+            }
+        }
+
+        public ListViewImageCellViewModel()
+        {
+            ListItems = new List<ListItem> { 
+                new ListItem {Source = "first.png", Title = "First", Description="1st item"}, 
+                new ListItem {Source = "second.png", Title = "Second", Description="2nd item"},
+                new ListItem {Source = "third.png", Title = "Third", Description="3rd item"}
+            };
+        }
+
+        public class ListItem : BindableObject
+        {
+            public string Source { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+        }
+    }
+}
+
