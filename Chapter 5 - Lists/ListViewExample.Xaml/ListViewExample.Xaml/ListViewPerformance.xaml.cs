e71c5e13321ff1aeb83b5b603e5cbb9a47d1334e
Alex Blount <alexb@alexs-macbook.local>
2015-06-23 13:41:36 +0100

    listview performance chapter 5

---
+++ Chapter 5 - Lists/ListViewExample.Xaml/ListViewExample.Xaml/ListViewPerformance.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace ListViewExample.Xaml
+{
+    public partial class ListViewPerformance : ContentPage
+    {
+        public ListViewPerformance()
+        {
+            InitializeComponent();
+
+            BindingContext = new ListViewPerformanceViewModel(DisplayAlert);
+        }
+    }
+
+    public class ListViewPerformanceViewModel : BindableObject
+    {
+        Func<string, string, string, Task> displayAlertAction;
+
+        List<ListItem> listItems;
+        public List<ListItem> ListItems
+        {
+            get
+            {
+                return listItems;
+            }
+            set
+            {
+                listItems = value;
+                OnPropertyChanged("ListItems");
+            }
+        }
+
+
+        ListItem selectedListItem;
+        public ListItem SelectedListItem
+        {
+            get
+            {
+                return selectedListItem;
+            }
+            set
+            {
+                selectedListItem = value;
+                OnPropertyChanged("SelectedListItem");
+                if (selectedListItem != null)
+                    displayAlertAction?.Invoke("Tapped", $"{selectedListItem.Title} was selected.", "OK"); 
+
+            }
+        }
+
+        public ListViewPerformanceViewModel(Func<string, string, string, Task> displayAlertAction)
+        {
+            this.displayAlertAction = displayAlertAction;
+
+            ListItems = Enumerable.Range(1, 500)
+                .Select(index => new ListItem { Source = "first.png", Title = $"{index}", Description = $"item number {index}", Price = $"${index}.00" })
+                .ToList();
+        }
+
+        public class ListItem : BindableObject
+        {
+            public string Source { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public string Price { get; set; }
+        }
+    }
+}
+
