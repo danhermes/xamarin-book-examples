b3fec5ad6d823905f8ca4116eaeaa08bd9f305bf
Alex Blount <alexb@alexs-macbook.local>
2015-06-24 06:56:50 +0100

    moving to code behind for eventing

--- Chapter 5 - Lists/ListViewExample.Xaml/ListViewExample.Xaml/ListViewDataModel.xaml.cs
+++ Chapter 5 - Lists/ListViewExample.Xaml/ListViewExample.Xaml/ListViewDataModel.xaml.cs
@@ -12,79 +12,62 @@ namespace ListViewExample.Xaml
         {
             InitializeComponent();
 
-            BindingContext = new ListViewDataModelViewModel(DisplayAlert);
+            BindingContext = new ListViewDataModelViewModel();
 
         }
 
-        void ListViewItemTapped (object sender, ItemTappedEventArgs e)
+        async void ListViewItemTapped (object sender, ItemTappedEventArgs e)
         {
-            ((ListViewDataModelViewModel)BindingContext).SelectedListItem = e.Item as ListViewDataModelViewModel.ListItem;
+            ListItem item = (ListItem)e.Item;
+            await DisplayAlert("Tapped", item.Title + " was selected.", "OK");
             ((ListView)sender).SelectedItem = null;
-        }
-    }
-
-    public class ListViewDataModelViewModel : BindableObject
-    {
-        readonly Func<string, string, string, Task> displayAlertAction;
-
-        List<ListItem> listItems;
-        public List<ListItem> ListItems
-        {
-            get
-            {
-                return listItems;
-            }
-            set
-            {
-                listItems = value;
-                OnPropertyChanged("ListItems");
-            }
-        }
-
-        ListItem selectedListItem;
-        public ListItem SelectedListItem
-        {
-            get
-            {
-                return selectedListItem;
-            }
-            set
-            {
-                selectedListItem = value;
-                OnPropertyChanged("SelectedListItem");
-                if (selectedListItem != null)
-                    displayAlertAction.Invoke("Tapped", selectedListItem.Title + " was selected.", "OK");
-            }
-        }
-
-        public ListViewDataModelViewModel(Func<string, string, string, Task> displayAlertAction)
-        {
-            this.displayAlertAction = displayAlertAction;
-
-            ListItems = new List<ListItem>
-            {
-                new ListItem
-                {
-                    Title = "First",
-                    Description = "1st Item"
-                },
-                new ListItem
-                {
-                    Title = "Second",
-                    Description = "2nd Item"
-                },
-                new ListItem
-                {
-                    Title = "Third",
-                    Description = "3rd Item"
-                },
-            };
         }
 
         public class ListItem
         {
+            public string Source { get; set; }
             public string Title { get; set; }
             public string Description { get; set; }
+            public string Price { get; set; }
+        }
+
+        public class ListViewDataModelViewModel : BindableObject
+        {
+            List<ListItem> listItems;
+            public List<ListItem> ListItems
+            {
+                get
+                {
+                    return listItems;
+                }
+                set
+                {
+                    listItems = value;
+                    OnPropertyChanged("ListItems");
+                }
+            }
+
+            public ListViewDataModelViewModel()
+            {
+                ListItems = new List<ListItem>
+                    {
+                        new ListItem
+                        {
+                            Title = "First",
+                            Description = "1st Item"
+                        },
+                        new ListItem
+                        {
+                            Title = "Second",
+                            Description = "2nd Item"
+                        },
+                        new ListItem
+                        {
+                            Title = "Third",
+                            Description = "3rd Item"
+                        },
+                    };
+            }
         }
     }
 }
--- Chapter 5 - Lists/ListViewExample.Xaml/ListViewExample.Xaml/ListViewImageCell.xaml.cs
+++ Chapter 5 - Lists/ListViewExample.Xaml/ListViewExample.Xaml/ListViewImageCell.xaml.cs
@@ -13,39 +13,41 @@ namespace ListViewExample.Xaml
 
             BindingContext = new ListViewImageCellViewModel();
         }
+
+        public class ListViewImageCellViewModel : BindableObject
+        {
+            List<ListItem> listItems;
+            public List<ListItem> ListItems
+            {
+                get
+                {
+                    return listItems;
+                }
+                set
+                {
+                    listItems = value;
+                    OnPropertyChanged("ListItems");
+                }
+            }
+
+            public class ListItem
+            {
+                public string Source { get; set; }
+                public string Title { get; set; }
+                public string Description { get; set; }
+            }
+
+            public ListViewImageCellViewModel()
+            {
+                ListItems = new List<ListItem> { 
+                    new ListItem {Source = "first.png", Title = "First", Description="1st item"}, 
+                    new ListItem {Source = "second.png", Title = "Second", Description="2nd item"},
+                    new ListItem {Source = "third.png", Title = "Third", Description="3rd item"}
+                };
+            }
+        }
     }
 
-    public class ListViewImageCellViewModel : BindableObject
-    {
-        List<ListItem> listItems;
-        public List<ListItem> ListItems
-        {
-            get
-            {
-                return listItems;
-            }
-            set
-            {
-                listItems = value;
-                OnPropertyChanged("ListItems");
-            }
-        }
 
-        public ListViewImageCellViewModel()
-        {
-            ListItems = new List<ListItem> { 
-                new ListItem {Source = "first.png", Title = "First", Description="1st item"}, 
-                new ListItem {Source = "second.png", Title = "Second", Description="2nd item"},
-                new ListItem {Source = "third.png", Title = "Third", Description="3rd item"}
-            };
-        }
-
-        public class ListItem : BindableObject
-        {
-            public string Source { get; set; }
-            public string Title { get; set; }
-            public string Description { get; set; }
-        }
-    }
 }
 
